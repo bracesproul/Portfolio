c2d8b2f2ae3ff40a1ee28e5cc4ef60b86a124ada
Brace Sproul <braceasproul@gmail.com>
2022-09-15 08:07:07 +0100

    added ip to first visit webhook

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -39,26 +39,36 @@ export default function Main(): JSX.Element {
         }
         console.log('new visitor');
         localStorage.setItem('isNewPortfolioVisitor', `${moment()}`);
-        const url = 'https://discord.com/api/webhooks/1019861600716476498/HLVayUdpqmFXR5sjHwx4KbAviuvCaqxaJ09m0nm8jNUu3m-fKKqU3dEn6HSXtB14fl8g'
-        const jsonPayload = {
-            embeds: [
-                {
-                    title: "New Visit",
-                    description: `Someone just visited the site!`,
-                    color: 16761035,
-                    footer: {
-                        text: 'Portfolio Bot'
-                    },
-                    timestamp: new Date().toISOString()
-                }
-            ]
-        }
-        axios.post(url, jsonPayload, {
-            headers: { 'Content-Type': 'application/json' },
-        })
-            .then(res => console.log(res))
-            .catch(err => console.log(err))
+        handleSendWebhook();
     }, [])
+
+    function handleSendWebhook() {
+        axios.get('https://geolocation-db.com/json/')
+            .then((res) => {
+                const url = 'https://discord.com/api/webhooks/1019861600716476498/HLVayUdpqmFXR5sjHwx4KbAviuvCaqxaJ09m0nm8jNUu3m-fKKqU3dEn6HSXtB14fl8g'
+                const jsonPayload = {
+                    embeds: [
+                        {
+                            title: "New Visit",
+                            description: `Someone just visited the site! IP: ***${res.data.IPv4}***`,
+                            color: 65280,
+                            footer: {
+                                text: 'Portfolio Bot'
+                            },
+                            timestamp: new Date().toISOString()
+                        }
+                    ]
+                }
+                axios.post(url, jsonPayload, {
+                    headers: { 'Content-Type': 'application/json' },
+                })
+                    .then(res => console.log(res))
+                    .catch(err => console.log(err))
+            })
+            .catch((err) => {
+                console.error('error getting location', err)
+            })
+    }
 
     return (
         <ThemeProvider theme={themeHook.theme}>
